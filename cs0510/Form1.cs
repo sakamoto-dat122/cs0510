70a1fe5433f2b124320a4fd639743b77eb0f5a44
haruto nakamura <104044075+noah-zty@users.noreply.github.com>
2024-05-17 14:01:53 +0900

    Initial commit

---
+++ cs0510/Form1.cs
@@ -0,0 +1,17 @@
+namespace cs0510
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "‰Ÿ‚³‚ê‚½";
+            button1.Left = 0;
+            button1.Visible = false;
+        }
+    }
+}
